3fffab631c91f409c2dd6f2ea8160d79797054b1
Asteises <p.nikitaev@gmail.com>
2023-08-29 18:42:40 +0300

    start: add-thread

--- src/main/java/ru/asteises/local_sites_searcher/controller/PageController.java
+++ src/main/java/ru/asteises/local_sites_searcher/controller/PageController.java
@@ -1,6 +1,5 @@
 package ru.asteises.local_sites_searcher.controller;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -17,20 +16,27 @@ import java.util.List;
 @RequestMapping("/api/search")
 public class PageController {
 
+    // TODO Можно сохранять данные о сайтах в БД, чтобы было проще искать по ним информацию. Правда как потом проверять, не произошли ли изменения на странице.
+
     @GetMapping("/all")
     public ResponseEntity<List<String>> getDataFromPage(@RequestParam List<String> urls, @RequestParam String word) throws IOException {
         // TODO приходит список сайтов и стринга которую нужно найти, для каждого сайта нужно создать свой поток поиска и сохранение в БД
         // TODO Будем возвращать список заголовков в которых нашли совпадение и ссылку на сайт
         String blogUrl = "https://bootlegbricks.ru/";
         Document doc = Jsoup.connect(blogUrl).get();
-        String pageTitle = doc.title();
-        Elements paragraphs = doc.getElementsByTag("p");
-        Elements anchors = doc.select("a");
+        String pageTitle = doc.title(); // Достаем тайтл страницы
+        Elements paragraphs = doc.getElementsByTag("p"); // Достаем параграфы
+        Elements anchors = doc.select("a"); // Достаем ссылки на странице
         return ResponseEntity.ok(anchors.stream()
                 .map(anchor -> anchor.attr("href"))
                 .toList());
     }
 
+    /**
+     * Метод ищет нужное словосочетание по сайту
+     * @return
+     * @throws IOException
+     */
     @GetMapping("/title")
     public ResponseEntity<String> getDataFromTitle() throws IOException {
         // TODO Сделать подключение к сайтам отдельным методомо
